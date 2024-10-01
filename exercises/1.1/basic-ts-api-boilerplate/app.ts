55ff658d49fede9a8c3b8d326d73350d20efdd9b
Josué Samba <josue.sambadianzungu@student.vinci.be>
2024-10-01 13:20:12 +0200

    new: ex1.2

--- exercises/1.1/basic-ts-api-boilerplate/app.ts
+++ exercises/1.1/basic-ts-api-boilerplate/app.ts
@@ -6,6 +6,15 @@ import filmRouter from "./routes/films";
 
 const app = express();
 
+let getCounter = 0;
+app.use((req, _res, next) => { //"_res" permet d'omettre le res
+  if (req.method === 'GET') {
+    getCounter++;
+    console.log('GET counter : ' +getCounter);
+  }
+  next(); 
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
